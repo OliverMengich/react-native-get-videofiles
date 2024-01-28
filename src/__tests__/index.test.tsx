0feffd5b4c8133a791e340d6cc19279f018d6001
OliverMengich <kemeioliver2000@gmail.com>
2024-01-28 21:14:13 +0300

    ! BREyAKING CHANGE: jest testing

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,22 @@
-it.todo('write a test');
+import RNGetVideofiles from "react-native-get-videofiles";
+jest.mock('react-native', () => {
+    const RNGetVideofiles = {
+        getAll: jest.fn(),
+    };
+    return {
+        Platform: {
+            OS: 'android',
+            select: jest.fn(),
+        },
+        NativeModules: {
+            RNGetVideofiles,
+        },
+    };
+});
+
+it('Gets all videos', async () => {
+    const videos = await RNGetVideofiles.getAll({
+        limit: 10,
+    });
+    expect(videos).toMatchSnapshot();
+});

9cdbae521baedfa9d788a8367a61d3e5998eaecd
OliverMengich <kemeioliver2000@gmail.com>
2024-02-03 05:53:43 +0300

    fix: types

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 
 import { StyleSheet, View, FlatList, Text } from 'react-native';
-import {  getAll, VideoFile } from 'react-native-get-videofiles';
-
+import RNVideoModule from 'react-native-get-videofiles';
+import type { VideoFile } from 'react-native-get-videofiles';
 export default function App() {;
   const [videos,setVideos] = React.useState<VideoFile[]>([]);
   React.useEffect(() => {
-    getAll().then(setVideos)
+    RNVideoModule.getAll({limit:4}).then(setVideos)
   }, []);
 
   return (
